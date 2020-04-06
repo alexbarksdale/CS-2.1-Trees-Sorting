14851fd39b70337878aeb66b7e7c4177f9c7f2a3
alexbarksdale <ale.barksdale@gmail.com>
2020-04-05 20:02:38 -0500

    Complexit

--- Code/sorting_iterative.py
+++ Code/sorting_iterative.py
@@ -3,8 +3,8 @@
 
 def is_sorted(items=list):
     """Return a boolean indicating whether given items are in sorted order.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
+    Running time: O(n) we're iterating over all elements
+    Memory usage: O(1) we're not creating new memory"""
     for i in range(0, len(items)-1):
         if items[i] > items[i + 1]:
             return False
@@ -14,10 +14,9 @@ def is_sorted(items=list):
 def bubble_sort(items=list, print_sorting=False):
     """Sort given items by swapping adjacent items that are out of order, and
     repeating until all items are in sorted order.
-    Running time:
-        - 
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
+    Running time: avg O(n^2) because we have an outer loop iterating over all
+    elements and an inner loop iterating over elements n - 1 - i
+    Memory usage: O(1) we're never creating new memory and doing it in place."""
 
     for i in range(len(items)):
         swapped = False
@@ -40,8 +39,8 @@ def bubble_sort(items=list, print_sorting=False):
 def selection_sort(items=list, print_sorting=False):
     """Sort given items by finding minimum item, swapping it with first
     unsorted item, and repeating until all items are in sorted order.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
+    Running time: O(n^2) we have two loops iterating over all the elements
+    Memory usage: O(1) we're never creating new memory and doing it in place."""
     for i in range(len(items)):
         min_val = i
 
@@ -63,8 +62,9 @@ def insertion_sort(items=list, print_sorting=False):
     # S/O to https://youtu.be/yCxV0kBpA6M for the help
     """Sort given items by taking first unsorted item, inserting it in sorted
     order in front of items, and repeating until all items are in order.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
+    Running time: O(n^2) because we have one loop iterating over all elements
+    and a nested loop that also iterates over n-1 elements.
+    Memory usage: O(1) we're never creating new memory and doing it in place."""
     for i in range(1, len(items)):
         extr_val = items[i]
         extr_loc = i - 1
@@ -86,9 +86,9 @@ def visualize_sort(items=list):
 
 
 if __name__ == '__main__':
-    T1 = [1, 36, 20, 15, 8, 0, 10]
+    T1 = [2, 1, 0, 4, 5]
     print('STARTING VALUES:', T1, '\n')
     print('Sorted?', is_sorted(T1))
-    bubble_sort(T1, True)
+    # bubble_sort(T1, True)
     # selection_sort(T1, True)
-    # insertion_sort(T1, True)
+    insertion_sort(T1, True)
